834bb705379aada5902e55c84228405cca8c6fb1
matthew <thisiswbb@gmail.com>
2022-02-25 11:22:20 +0000

    Created baseline class files, finished city.java

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,28 @@
+package com.napier.sem;
+
+/**
+ * Represents a city
+ */
+public class City {
+
+    /**
+     * Represents the city id
+     */
+    public int city_id;
+
+    /**
+     * City's name
+     */
+    public String city_name;
+
+    /**
+     * City's district
+     */
+    public String district;
+
+    /**
+     * the population of the city
+     */
+    public int population;
+
+}
