d10e45b47cb160f6385ffd3ad77a18b165fba665
matthew <thisiswbb@gmail.com>
2022-03-23 19:20:25 +0000

    fixed pom.xml and app to connect properly to database on github actions

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -13,39 +13,31 @@ public class App
     /**
      * Connect to the MySQL database.
      */
-    public void connect(String location, int delay)
-    {
-        try
-        {
+    public void connect(String location, int delay) {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(delay);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://"+ location + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://" + location
+                                + "/world?allowPublicKeyRetrieval=true&useSSL=false",
+                        "root", "example");
                 System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+            } catch (SQLException sqle) {
+                System.out.println("Failed to connect to database attempt " +                                  Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
@@ -80,10 +72,11 @@ public class App
         App a = new App();
 
         // Connect to database
-        if(args.length < 1) {
-            a.connect("localhost:33060", 0);
+
+        if(args.length < 1){
+            a.connect("localhost:33060", 30000);
         }else{
-            a.connect("db3306",30000);
+            a.connect(args[0], Integer.parseInt(args[1]));
         }
         ArrayList<Country> country = a.getCountryByRegion("Eastern Europe");
       //  ArrayList<Country> countryByContinent = a.Top5CountriesInAContinent("5", "Europe");
